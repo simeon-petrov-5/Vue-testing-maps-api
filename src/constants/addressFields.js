86177f7363e849030a07be4e9ddbce5674d960ce
Simeon Petrov <s.petrov@blubito.com>
2020-10-15 12:56:18 +0300

    initial

---
+++ src/constants/addressFields.js
@@ -0,0 +1,18 @@
+ const addressFields = [
+    "administrative_area_level_1",
+    "administrative_area_level_2",
+    "administrative_area_level_3",
+    "administrative_area_level_4",
+    "administrative_area_level_5",
+    "continent",
+    "country",
+    "locality",
+    "natural_feature",
+    "neighborhood",
+    "postal_code",
+    "route",
+    "street_address",
+    "street_number",
+  ]
+
+ export default addressFields;
